8e51b39ac9114586d07bb65fdfb7fbdc1e23c511
subhashreebarik123 <bariksubhashree177@gmail.com>
2024-05-14 14:27:48 +0530

    ded

--- src/main/java/com/java/rest/restwebservicehello/print/java/Samplejava.java
+++ src/main/java/com/java/rest/restwebservicehello/print/java/Samplejava.java
@@ -4,6 +4,6 @@ public class Samplejava {
     public static void main(String[] args) {
         int a = 30;
         int b = 40;
-        System.out.println(a++);
+        System.out.println(b++);
     }
 }
